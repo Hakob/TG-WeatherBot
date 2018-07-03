b1cc4659213d5a766f812e30ee7d6cb466b43015
Hakob Arakelyan <hakoaraqelyan@gmail.com>
2018-07-03 22:47:09 +0400

    Added Logger

--- main.py
+++ main.py
@@ -6,6 +6,17 @@ from time import sleep
 
 from BotHandler import BotHandler
 from WeatherHandler import WeatherHandler
+import logging
+import sys
+
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+root.addHandler(ch)
 
 weather_api_key = '<open weather map api_key>'
 tg_token = '<telegram bot token>'
